fe2c49e54fa448c928f9c901b638486ea777f7ec
Gustavo Gomes <dev.gustavogomes@gmail.com>
2020-04-10 18:14:40 -0300

    Trazendo dados dinamicamente

---
+++ backend/data.js
@@ -0,0 +1,23 @@
+module.exports = [
+  {
+    id: "starter",
+    image_url: "https://skylab.rocketseat.com.br/static/64c237ccff807c054339a62d53b4b402.svg",
+    content: "Torne-se um programador desejado no mercado com esses cursos gratuitos",
+    info: "5 Módulos",
+    price: "Free"
+  },
+  {
+    id: "launchbase",
+    image_url: "https://skylab.rocketseat.com.br/static/0828532024cb46921a6b5e941f8d788d.svg",
+    content: "Domine a programação do zero e tenha as melhores oportunidades do mercado",
+    info: "7 Módulos",
+    price: "Free"
+  },
+  {
+    id: "gostack",
+    image_url: "https://skylab.rocketseat.com.br/static/83a178a0653dab1d55e2ed7946465975.svg",
+    content: "Torne-se um programador desejado no mercado com esses cursos gratuitos",
+    info: "12 Módulos",
+    price: "Free"
+  },
+]
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const nunjucks = require('nunjucks')
 
+const pages = require('./data')
+
 const server = express()
 
 server.use(express.static("../public"))
@@ -11,16 +13,33 @@ nunjucks.configure("../views", {
   express: server
 })
 
-server.get("/", (request, response) => {
-  response.render("courses")
+server.get("/", (req, res) => {
+  return res.render("courses", { items: pages })
 })
 
-server.get("/about", (request, response) => {
-  response.render("about")
+server.get("/about", (req, res) => {
+  const about = {
+    avatar_url: "https://avatars0.githubusercontent.com/u/28929274?s=200&v=4",
+    name: "Rocketseat",
+    description: "Plataforma de educação em tecnologia 🚀",
+    technologies: [
+      { name: "Javascript" }, 
+      { name: "React" }, 
+      { name: "React-Native" }, 
+      { name: "NodeJS" }, 
+    ],
+    links: [
+      { name: "Github", url: "https://github.com/Rocketseat" },
+      { name: "Intagram", url: "https://www.instagram.com/rocketseat_oficial/" },
+      { name: "Facebook", url: "https://www.facebook.com/rocketseat/" },
+    ]
+  }
+
+  return res.render("about", { about })
 })
 
-server.use((request, response) => {
-  response.status(404).render("not-found")
+server.use((req, res) => {
+  return res.status(404).render("not-found")
 })
 
 server.listen(5000, () => {
